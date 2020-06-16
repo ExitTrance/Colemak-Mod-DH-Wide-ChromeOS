c7468dbb30608d36a8e6466d01ef84fbf3dfe851
ExitTrance <dearz@hotmail.de>
2020-06-16 15:27:48 +0100

    Fix for newer Chrome versions and addition of Extend Mod.

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
-# Colemak Mod-DH Wide for ChromeOS (QWERTY)
+# Colemak Mod-DH Wide (+ Extend Mod) for ChromeOS (QWERTY)
 
-Little extension to enable the Colemak Mod-DH Wide layout for ChromeOS with specifically a QWERTY UK Layout. Tested and working on the Dell CB13 7310. Does not require the ChromeOS Developer Mode to be turned on, as far as I know. This can be modified to accommodate any layout, as it simply converts binded keys to some user defined text when typed. As it is an extension, it does not work on the lock/login screen. According to [Extra Keyboards for ChromeOS](https://github.com/google/extra-keyboards-for-chrome-os), and through own testing using [this](https://codepen.io/Sohail05/pen/yOpeBm) codepen, I have found that ChromeOS defaults to your main keyboard for any password fields.
+Chrome extension to enable the Colemak Mod-DH Wide layout for ChromeOS with specifically a QWERTY UK Layout. Tested and working on the Dell CB13 7310 on ChromeOS 84.04147.37 (Beta). Does not require the ChromeOS Developer Mode to be turned on, as far as I know. This can be modified to accommodate any layout, as it simply converts binded keys to some user defined text when typed. As it is an extension, it does not work on the lock/login screen. According to [Extra Keyboards for ChromeOS](https://github.com/google/extra-keyboards-for-chrome-os), and through own testing using [this](https://codepen.io/Sohail05/pen/yOpeBm) codepen, I have found that ChromeOS defaults to your main keyboard for any password fields.
 
+I doubt anyone will use this, but this is more so for keeping save the effort I made to make Colemak work on ChromeOS. Sadly you are NOT able to activate Search, Assistant, or any of the ChromeOS wide shortcuts like switching Desks yet, which would have made this mod so much more enjoyable. However, you are able to use one click Copy, Cut, and Paste (Undo does not work for some odd reason.)!! Also, moving Ctrl, Shift (and Backspace) to the homerow or close to it, has allowed me to make working with text a lot more comfortable.
 
 ### Mod-DH Wide Layout
+Choice of Extend key is Capslock here which has been remapped to be in the location of the Search Key on ChromeOS. Too lazy to provide a graphic of the Extend mapping since it is very personal. 
 
 ![Picture of the Layout](https://colemakmods.github.io/mod-dh/gfx/mod_dh_keyb_iso_wide.png)
 
 
 ### How to use it
 
-1. Place `main.js` and `manifest.json` in a folder. 
+1. Place `background.js` and `manifest.json` in a folder. 
 2. Go to `chrome://extensions/` and enable the 'Developer mode'.
 3. Click on 'Load unpacked' and select the folder where both files are located at.
 4. Go to the following link `chrome://settings/inputMethods` or `chrome://settings/languages` and navigate to 'Input method' -> 'Manage input methods'.
@@ -20,19 +22,13 @@ Little extension to enable the Colemak Mod-DH Wide layout for ChromeOS with spec
 
 ### How to modify the layout
 
-If you want to modify the layout for whatever purpose, you can do so by modifying the 'chars' object inside `main.js`. For example, you could switch the Z and Y keys around in the case of a QWERTZ keyboard.
-
+If you want to modify the layout for whatever purpose, you can do so by modifying the 'dic' object inside `background.js`. For simple remapping of non-shifted and shifted keys, simply use the first two entries in the array. For extended mapping, use 3rd entry as the actual key to be pressed, and the 4th entry whether shift/alt/ctrl are pressed during the key extended key. Note that for remapping Control/Shift/Alt, you have to have the respective key set to pressed as well as shown below.
 ```javascript
-//QWERTZ
-// "Actual Key" : "Desired Key"
+// "Actual Key" : ["non-shift", "shifted", "extended mapping key", "shiftKey, altKey, ctrlKey"]
 
-var chars = {
-...
-  "Z": "{",   "z": "[",
-...
-  "Y": "X",   "y": "x",
-...
-}
+"KeyD": ["s", "S", null, null],
+"KeyF": ["t", "T", "Control", "ctrlKey"],
+
 ```
 
 ### Credit
---
+++ background.js
@@ -0,0 +1,172 @@
+/*
+Copyright 2018 The Extra Keyboards for Chrome OS Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+var mod_keys  = {shiftKey: false, ctrlKey: false}
+ 
+var contextID = 0;
+
+var dic = {
+    "Digit1": ["!", "1", null, null],
+    "Digit2": ["\"", "2", null, null],
+    "Digit3": ["£", "3", null, null],
+    "Digit4": ["$", "4", null, null],
+    "Digit5": ["%", "5", null, null],
+    "Digit6": ["=", "+", null, null],
+    "Digit7": ["^", "6", null, null],
+    "Digit8": ["&", "7", null, null],
+    "Digit9": ["*", "8", null, null],
+    "Digit0": ["(", "9", null, null],
+    "Minus":  [")", "0", null, null],
+    "Equal":  ["-", "_", null, null],
+
+
+    "KeyQ": ["q", "Q", null, null],
+    "KeyW": ["w", "W", null, null],
+    "KeyE": ["f", "F", "Escape", null],
+    "KeyR": ["p", "P", null, null],
+    "KeyT": ["b", "B", null, null],
+    "KeyY": ["[", "{", null, null],
+    "KeyU": ["j", "J", null, null],
+    "KeyI": ["l", "L", null, null],
+    "KeyO": ["u", "U", "ArrowUp", null],
+    "KeyP": ["y", "Y", null, null],
+    "BracketLeft": [";", ":", null, null],
+    "BracketRight": ["/", "?", null, null],
+
+    "KeyA": ["a", "A", null, null],
+    "KeyS": ["r", "R", null, null],
+    "KeyD": ["s", "S", "Shift", "shiftKey"],
+    "KeyF": ["t", "T", "Control", "ctrlKey"],
+    "KeyG": ["g", "G", "KeyA", null],
+    "KeyH": ["]", "}", null, null],
+    "KeyJ": ["k", "K", null, null],
+    "KeyK": ["n", "N", "ArrowLeft", null],
+    "KeyL": ["e", "E", "ArrowDown", null],
+    "Semicolon": ["i", "I", "ArrowRight", null],
+    "Quote": ["o", "O", "Shift", "shiftKey"],
+    //~ and # on UK keyboards
+    "Backslash": ["'", "@", null],
+
+    "IntlBackslash": ["z", "Z", "Undo", null],
+    "KeyZ": ["x", "X", "Cut", null],
+    "KeyX": ["c", "C", "Copy", null],
+    "KeyC": ["d", "D", "Paste", null],
+    "KeyV": ["v", "V", "Backspace", null],
+    "KeyB": ["\\", "|", "LaunchAssistant", null],
+    "KeyN": ["#", "~", null, null],
+    "KeyM": ["m", "M", null, null],
+    "Comma": ["h", "H", null, null],
+    "Period": [",", "<", null, null],
+    "Slash": [".", ">", null, null],
+    
+};
+
+
+
+chrome.input.ime.onFocus.addListener(
+    function(context) {
+        contextID = context.contextID;
+    }
+);
+
+chrome.input.ime.onBlur.addListener(() => {
+    contextID = -1;
+})
+
+
+
+// TODO: Add EXTEND functionality
+//       Requires handling of both keydown AND keyup
+chrome.input.ime.onKeyEvent.addListener(
+
+
+    function(engineID, keyData) {
+        var handled = false;
+        //console.log(keyData)
+        if (keyData.type == "keydown") {
+        
+            if (dic[keyData.code]) {
+                let shifted = false ^ keyData.shiftKey;
+                let emit = dic[keyData.code][shifted];
+
+                if (keyData.capsLock) {
+                    original_key = keyData.code
+                    keyData.code = dic[keyData.code][2];
+                    if (keyData.code != null) {
+                        keyData.key = ""
+
+                        if(dic[original_key][3] != null) {
+                            mod_keys[dic[original_key][3]] = true
+                        }
+                        
+                        keyData.ctrlKey  = keyData.ctrlKey   || mod_keys["ctrlKey"] 
+                        keyData.shiftKey = keyData.shiftKey  || mod_keys["shiftKey"] 
+                        console.log(keyData)
+                        chrome.input.ime.sendKeyEvents({
+                            "contextID": contextID,
+                            "keyData": [keyData]
+                        });
+                        
+                       
+                        
+                    }
+
+                } else if (emit != null && contextID != 0) {
+                    chrome.input.ime.commitText({
+                        "contextID": contextID,
+                        "text": emit,
+                    }, () => {
+                        if (chrome.runtime.lastError) {
+                            console.error('Error committing text:', chrome.runtime.lastError);
+                            return;
+                        }
+                    });
+                }
+
+                handled = true;
+            }
+
+
+        }else if(keyData.type == "keyup"){
+            if(dic[keyData.code] && keyData.capsLock ){
+                original_key = keyData.code
+                keyData.code = dic[keyData.code][2];
+                
+                if(keyData.code != null) {
+                    if(dic[original_key][3] != null) {
+                        mod_keys[dic[original_key][3]] = false
+                    }
+
+                    keyData.ctrlKey  = keyData.ctrlKey   || mod_keys["ctrlKey"] 
+                    keyData.shiftKey = keyData.shiftKey  || mod_keys["shiftKey"] 
+                    
+                    console.log(keyData)
+                    chrome.input.ime.sendKeyEvents({
+                        "contextID": contextID,
+                        "keyData": [keyData]
+                    });
+                }
+              
+                
+                handled = true;
+            }
+            
+            
+        }
+        
+        
+        
+        return handled;
+    });
